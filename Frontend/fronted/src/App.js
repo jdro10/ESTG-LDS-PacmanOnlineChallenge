60e4a51a35acce3ff20d97b34edf6365f2a9210b
vmsb1998 <vmsb1998@gmail.com>
2019-11-15 20:42:10 +0000

    Style for login page

--- Frontend/fronted/src/App.js
+++ Frontend/fronted/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 
+import logo from "./assets/logo.png";
+
 function App() {
   return (
-    <div className="login-container">
-      <form>
+    <div className="container">
+      <form className="login-container">
+        <img src={logo} alt="It a match" />
         <input placeholder="Email" />
         <input placeholder="Password" type="password" />
 
         <button type="submit">Login</button>
+        <button type="submit">Register</button>
       </form>
-      <button type="submit">Register</button>
     </div>
   );
 }
