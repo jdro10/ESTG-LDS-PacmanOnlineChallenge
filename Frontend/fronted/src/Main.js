7ec1efab76710cea84e821769eff4dbdbdd3d39c
vmsb1998 <vmsb1998@gmail.com>
2019-11-23 17:40:55 +0000

    test with an example of social media

---
+++ Frontend/fronted/src/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function src() {
+  return (
+    <div className="footer-container">
+      <div className="social">
+        <a href="https://github.com/RAJ66">
+          <img src="https://img.icons8.com/material-rounded/52/000000/github.png" />
+        </a>
+      </div>
+    </div>
+  );
+}
