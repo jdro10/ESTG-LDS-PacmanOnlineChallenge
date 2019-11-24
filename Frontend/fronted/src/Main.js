3b3e21c62562c2936859f2ae867c1fbf27201b6e
vmsb1998 <vmsb1998@gmail.com>
2019-11-24 17:05:16 +0000

    footer complete

--- Frontend/fronted/src/Main.js
+++ Frontend/fronted/src/Main.js
@@ -1,23 +1,25 @@
 import React from "react";
-
+import "./Main.css";
 // import { Container } from './styles';
 
 export default function src() {
   return (
-    <div className="footer-container">
-      <div className="social">
-        <a href="https://github.com/RAJ66">
-          <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" />
-        </a>
-        <a href="https://github.com/RAJ66">
-          <img src="https://img.icons8.com/material/50/000000/instagram-new--v1.png" />
-        </a>
-        <a href="https://github.com/RAJ66">
-          <img src="https://img.icons8.com/ios-filled/50/000000/twitter-circled.png" />
-        </a>
-        <a href="https://github.com/RAJ66">
-          <img src="https://img.icons8.com/ios-filled/50/000000/exit.png" />
-        </a>
+    <div className="container">
+      <div className="footer-container">
+        <div className="social">
+          <a href="https://github.com/RAJ66">
+            <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" />
+          </a>
+          <a href="https://github.com/RAJ66">
+            <img src="https://img.icons8.com/material/50/000000/instagram-new--v1.png" />
+          </a>
+          <a href="https://github.com/RAJ66">
+            <img src="https://img.icons8.com/ios-filled/50/000000/twitter-circled.png" />
+          </a>
+          <a href="https://github.com/RAJ66">
+            <img src="https://img.icons8.com/ios-filled/50/000000/exit.png" />
+          </a>
+        </div>
       </div>
     </div>
   );
