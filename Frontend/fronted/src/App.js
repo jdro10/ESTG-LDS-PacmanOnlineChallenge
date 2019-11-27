df0b30a114f32a2b90dd7de19b49ce7bfb320e2f
vmsb1998 <vmsb1998@gmail.com>
2019-11-27 10:37:34 +0000

    fetch information from email to input

--- Frontend/fronted/src/App.js
+++ Frontend/fronted/src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import api from "./services/api";
 
 import logo from "./assets/logo.png";
 
 function App() {
+  const [email, setEmail] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Teste");
+    console.log(email);
   }
 
   return (
     <div className="container">
       <form className="login-container" onSubmit={handleSubmit}>
         <img src={logo} alt="It a match" />
-        <input placeholder="Email" />
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
         <input placeholder="Password" type="password" />
 
         <button type="submit" className="glow-on-hover">
