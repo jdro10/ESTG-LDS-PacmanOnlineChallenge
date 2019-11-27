1fd73e4f1cde278daaa18d13579190533618480b
vmsb1998 <vmsb1998@gmail.com>
2019-11-27 10:12:02 +0000

    create api services

--- Frontend/fronted/src/App.js
+++ Frontend/fronted/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./App.css";
+import api from "./assets/api";
 
 import logo from "./assets/logo.png";
+import api from "./services/api";
 
 function App() {
   return (
