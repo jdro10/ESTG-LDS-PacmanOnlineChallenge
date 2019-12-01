2ddcf44bbdcd58004b151f283ae59bc50b92efde
Jorge Oliveira <jorgedroliveira10@gmail.com>
2019-12-01 22:46:13 +0000

    more backend restrictions

--- Backend/API/Controllers/UserController.cs
+++ Backend/API/Controllers/UserController.cs
@@ -37,13 +37,20 @@ namespace API.Controllers
         [HttpPost]
         public ActionResult<User> Create(User user)
         {
-            Regex regex = new Regex(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$");
-            Match matchEmail = regex.Match(user.Email);
+            Regex regexEmail = new Regex(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$");
+            Match matchEmail = regexEmail.Match(user.Email);
+
+            Regex regexPassword = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+            Match matchPassword = regexPassword.Match(user.Password);
+
+            //password must have a minimum of 8 characters, at least one number and one letter
 
             var usernameExists = _userService.GetByName(user.Username);
             var emailExists = _userService.GetByEmail(user.Email);
+            var newUsernameLength = user.Username.Length;
 
-            if(usernameExists == null && emailExists == null && matchEmail.Success)
+            if (usernameExists == null && emailExists == null
+                && matchEmail.Success && newUsernameLength >= 3 && matchPassword.Success)
             {
                 _userService.Create(user);
             }
@@ -51,7 +58,7 @@ namespace API.Controllers
             {
                 return NoContent();
             }
-            
+
             return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
         }
 
