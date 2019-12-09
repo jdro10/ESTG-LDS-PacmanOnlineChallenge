294a85211ba94c4d0d1d555bfcdab0567490642f
Jorge Oliveira <jorgedroliveira10@gmail.com>
2019-12-09 21:13:20 +0000

    added jwt authentication

--- Backend/API/Services/UserService.cs
+++ Backend/API/Services/UserService.cs
@@ -17,6 +17,28 @@ namespace API.Services
             _users = database.GetCollection<User>(settings.UserCollectionName);
         }
 
+        public User Authenticate(string username, string password)
+        {
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+
+            var user = GetByName(username);
+
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(password != user.Password)
+            {
+                return null;
+            }
+
+            return user;
+        }
+
         public List<User> Get() => _users.Find(user => true).ToList();
 
         public User Get(string id) =>
