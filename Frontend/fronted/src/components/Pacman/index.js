a25d8de227233ad5776df0399dfae44672e5236e
vmsb1998 <vmsb1998@gmail.com>
2019-12-09 17:43:00 +0000

    add eyes

--- Frontend/fronted/src/components/Pacman/index.js
+++ Frontend/fronted/src/components/Pacman/index.js
@@ -5,8 +5,8 @@ import "./styles.css";
 export default function Pacman() {
   return (
     <div className="pacman">
-      <div className="pacman__eye"></div>
-      <div class="pacman__mouth"></div>
+      <div className="pacmanEye"></div>
+      <div class="pacmanMouth"></div>
     </div>
   );
 }
