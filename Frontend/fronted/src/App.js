bf17589614dfb960f21f80369ac1abfffb6df382
vmsb1998 <vmsb1998@gmail.com>
2019-12-10 18:48:14 +0000

    general color settings and login page color changes

--- Frontend/fronted/src/App.js
+++ Frontend/fronted/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+
 import "./App.css";
+import "./setting/colors.css";
 
 import Routes from "./routes";
-import Pacman from "./components/Pacman/index";
 
 function App() {
   return (
     <>
-      <Pacman />
+      <Routes />
     </>
   );
 }
