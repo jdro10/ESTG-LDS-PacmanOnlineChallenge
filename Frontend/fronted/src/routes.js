29f9b7c4bc21bb793ba1517f5bc27141f5d38fdb
vmsb1998 <vmsb1998@gmail.com>
2019-12-11 13:32:24 +0000

    route to register

--- Frontend/fronted/src/routes.js
+++ Frontend/fronted/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Pacman from "./components/Pacman/index";
+import Register from "./pages/Register/index";
 
 export default function Routes() {
   return (
@@ -11,6 +12,8 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/register" component={Register} />
+
         <Route path="/pacman" component={Pacman} />
       </Switch>
     </BrowserRouter>
