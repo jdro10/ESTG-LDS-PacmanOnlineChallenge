fe538ecfee4ee994386d82863eb7b716faa94d68
vmsb1998 <vmsb1998@gmail.com>
2019-12-11 19:06:30 +0000

    add pacman animation

--- Frontend/fronted/src/pages/Register/index.js
+++ Frontend/fronted/src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import "./styles.css";
+import Pacman from "./../../components/Pacman/index";
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -10,6 +11,7 @@ export default function Register() {
   return (
     <div className="container-register">
       <form className="register-container">
+        <Pacman className="register-pacman" />
         <input
           placeholder="UserName"
           value={user}
