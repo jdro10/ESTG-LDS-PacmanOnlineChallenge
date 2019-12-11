5813f7e1f82eb77400dc5ed0526ce987d51f9ff7
vmsb1998 <vmsb1998@gmail.com>
2019-12-11 13:45:29 +0000

    initial version of register

--- Frontend/fronted/src/pages/Register/index.js
+++ Frontend/fronted/src/pages/Register/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
   return (
-    <div>
-      <h1>teste</h1>
+    <div className="container-register">
+      <form className="register-container">
+        <input
+          placeholder="UserName"
+          value={user}
+          onChange={event => setUser(event.target.value)}
+        />
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={pass}
+          onChange={event => setPass(event.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
