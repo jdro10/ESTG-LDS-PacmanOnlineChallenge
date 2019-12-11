eadd7ca7a7ff92759199f2eb10eaab7b45cce2b4
vmsb1998 <vmsb1998@gmail.com>
2019-12-11 10:21:03 +0000

    change the rest of the colors

--- Frontend/fronted/src/routes.js
+++ Frontend/fronted/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Pacman from "./components/Pacman/index";
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/pacman" component={Pacman} />
       </Switch>
     </BrowserRouter>
   );
