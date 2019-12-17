5328499663c92aa4db1b205a35e7f3a5c7f1312f
vmsb1998 <vmsb1998@gmail.com>
2019-12-17 13:19:47 +0000

    add button

--- Frontend/frontend/src/pages/Login/index.js
+++ Frontend/frontend/src/pages/Login/index.js
@@ -41,15 +41,19 @@ export default function Login({ history }) {
           onChange={event => setPass(event.target.value)}
         />
 
-        <button type="submit" className="glow-on-hover">
+        <button type="submit" className="glow-on-hover basebutton">
           Login
         </button>
         <button
           type="reset"
           onClick={handleClickRegister}
-          className="glow-on-hover"
+          className="glow-on-hover basebutton"
         >
           Register
+        </button>
+
+        <button type="reset" className="forgotButton ">
+          Forgot account?
         </button>
       </form>
     </div>
