c654a3689ca73c48cd9211d21036b9dba74fa381
vmsb1998 <vmsb1998@gmail.com>
2019-12-21 16:57:32 +0000

    Pacman test complete

---
+++ Frontend/frontend/src/__tests__/Pacman.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import Pacman from "../components/Pacman/index";
+
+describe("Testing Pacman Component", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(<Pacman />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
