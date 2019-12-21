3f7541e5440f58a6d88718d5fac608d7de5237cc
vmsb1998 <vmsb1998@gmail.com>
2019-12-21 14:12:16 +0000

    Snapshot example

--- Frontend/frontend/src/App.test.js
+++ Frontend/frontend/src/App.test.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "./App";
+import Login from "./pages/Login/index.js";
+import ShallowRenderer from "react-test-renderer/shallow"; // ES6
+import toJson from "enzyme-to-json";
 
 describe("Testing Login Component", () => {
   it("should render correctly", () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(
+      <div className="my-component">
+        <strong>ola World!</strong>
+      </div>
+    );
 
-    expect(wrapper).toMatchSnapshot();
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it("knows that 2 and 2 make 4", () => {
