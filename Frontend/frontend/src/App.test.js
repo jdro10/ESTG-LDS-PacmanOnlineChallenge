e0df82b1555130a7ecea0395c170c928b8df56f9
vmsb1998 <vmsb1998@gmail.com>
2019-12-21 11:51:08 +0000

    working sample tests

--- Frontend/frontend/src/App.test.js
+++ Frontend/frontend/src/App.test.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Login from "../src/pages/Login/index";
+import App from "./App";
 
 describe("Testing Login Component", () => {
   it("should render correctly", () => {
-    const wrapper = shallow(<Login />);
+    const wrapper = shallow(<App />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("knows that 2 and 2 make 4", () => {
+    expect(2 + 2).toBe(4);
   });
 });
