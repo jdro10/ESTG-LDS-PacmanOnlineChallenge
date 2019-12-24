8deccb983e70ea9bb995ae6be4c74a0040469d8a
Jorge Oliveira <jorgedroliveira10@gmail.com>
2019-12-24 17:23:47 +0000

    added userstats dto to return only needed info

--- Backend/API/Controllers/RankController.cs
+++ Backend/API/Controllers/RankController.cs
@@ -3,6 +3,7 @@ using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
+using API.Dtos;
 using System;
 
 namespace API.Controllers
@@ -14,6 +15,7 @@ namespace API.Controllers
     {
         private readonly RankService _rankService;
         private List<User> _userRank;
+        private List<UserStatsDto> _userStatsDto;
         private readonly UserService _userService;
         private readonly LevelService _levelService;
 
@@ -21,6 +23,7 @@ namespace API.Controllers
         {
             _rankService = rankService;
             _userRank = new List<User>();
+            _userStatsDto = new List<UserStatsDto>();
             _userService = userService;
             _levelService = levelService;
         }
@@ -38,9 +41,23 @@ namespace API.Controllers
             return _userRank;
         }
 
+        private ActionResult<List<UserStatsDto>> OrderByScore2(UserStatsDto[] usdto)
+        {
+            var users = usdto;
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                _userStatsDto.Add(users[i]);
+            }
+
+            _userStatsDto.Sort((x, y) => y.Score.CompareTo(x.Score));
+
+            return _userStatsDto;
+        }
+
         [AllowAnonymous]
         [HttpGet("allusers")]
-        public ActionResult<List<User>> UsersInfo()
+        public ActionResult<List<UserStatsDto>> UsersInfo()
         {
             var userRanks = OrderByScore().ToArray();
             List<User> userList = new List<User>();
@@ -53,7 +70,25 @@ namespace API.Controllers
                 userList.Add(userRanks[i]);
             }
             
-            return _userService.Get();
+            var userRanks2 =  _userService.Get().ToArray();
+
+            UserStatsDto[] userRanksDto = new UserStatsDto[userRanks2.Length];
+
+            for(int i = 0; i < userRanks2.Length; i++)
+            {
+                userRanksDto[i] = new UserStatsDto();
+                userRanksDto[i].Id = userRanks2[i].Id;
+                userRanksDto[i].Username =userRanks2[i].Username;
+                userRanksDto[i].Email = userRanks2[i].Email;
+                userRanksDto[i].Level = userRanks2[i].Level;
+                userRanksDto[i].Score = userRanks2[i].Score;
+                userRanksDto[i].Rank = userRanks2[i].Rank;
+                userRanksDto[i].dailyChallenges = userRanks2[i].dailyChallenges;
+            }
+
+            var listToReturn = this.OrderByScore2(userRanksDto);
+
+            return listToReturn;
         }
 
         [HttpGet]
