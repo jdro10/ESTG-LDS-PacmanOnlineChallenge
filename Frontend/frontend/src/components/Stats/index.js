6ed2c2aa85d29e8839f0b3a0127483d06f9450dc
vmsb1998 <vmsb1998@gmail.com>
2019-12-25 16:29:52 +0000

    bug fix

--- Frontend/frontend/src/components/Stats/index.js
+++ Frontend/frontend/src/components/Stats/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "./styles.css";
-import ImageLevel from "./../ImageLevel/index";
 import ProgressBar from "./../ProgressBar/index";
 
 export default function Stats({ nivel, score, rank }) {
