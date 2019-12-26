6ccb3e8a4f8c588e873605d4413ea631dcb685f2
hugocorreia <hrscorreia@gamil.com>
2019-12-26 01:34:11 +0000

    a little bit

--- Game/Menu/Multi.py
+++ Game/Menu/Multi.py
@@ -5,8 +5,22 @@ from MenuInstrucoes import start_draw1
 #clock.tick(5)
 pygame.init()
 
+#windows settings
 display_width = 800
 display_height = 600
 
+#colors
+black = (0,0,0)
+white = (255,255,255)
+
+#window configuration
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Choose character')
+
+#game config
+clock = pygame.time.Clock()
+Running = True
+
+#images
+pacman = pygame.image.load('pacman.png')
+ghost = pygame.image.load('vermelho.png')
