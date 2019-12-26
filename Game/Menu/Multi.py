c1ec7336a0d5c85b7cd6d13bbb1a776c8cab53c5
hugocorreia <hrscorreia@gamil.com>
2019-12-26 02:17:54 +0000

    almost done!

--- Game/Menu/Multi.py
+++ Game/Menu/Multi.py
@@ -12,6 +12,14 @@ display_height = 600
 #colors
 black = (0,0,0)
 white = (255,255,255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+TEXT_SIZE_MENU = 30
+FONT_MENU = 'PacFont'
+TITLE_SIZE = 26
+TEXT_SIZE_GAME = 13
+FONT_GAME = 'arial black'
 
 #window configuration
 gameDisplay = pygame.display.set_mode((display_width,display_height))
@@ -24,12 +32,27 @@ Running = True
 #images
 pacman = pygame.image.load('pacman.png')
 ghost = pygame.image.load('vermelho.png')
+ghost = pygame.transform.scale(ghost, (180, 180))
 
-def pac(x,y):
-    gameDisplay.blit(pacman, (x,y))
+
+
+
+
+
+xis = (display_width * 0.65)
+ypslon = (display_height * 0.38)
 
 x =  (display_width * 0.1)
 y = (display_height * 0.35)
+
+
+def draw_text(message, screen, position, size, font_type, color):
+    font = pygame.font.SysFont(font_type, size)
+    text = font.render(message, False, color)
+    text_size = text.get_size()
+    position[0] = position[0] - text_size[0] // 2
+    screen.blit(text, position)
+
 
 while Running:
     for event in pygame.event.get():
@@ -37,7 +60,10 @@ while Running:
             pygame.quit()
 
     gameDisplay.fill(black)
-    pac(x, y)
+    gameDisplay.blit(pacman, (x, y))
+    gameDisplay.blit(ghost, (xis, ypslon))
+    draw_text('CHOOSE YOUR CHARACTER !', gameDisplay, [display_width // 2, display_height // 2 - 140], TITLE_SIZE, FONT_MENU, white)
+
 
     pygame.display.update()
     clock.tick(60)
