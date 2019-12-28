c3b425d82b7c3dc979518eeb717a17cf9182728a
hugocorreia <hrscorreia@gamil.com>
2019-12-28 01:21:57 +0000

    Menu personagens concluido

--- Game/Menu/Multi.py
+++ Game/Menu/Multi.py
@@ -1,54 +1,10 @@
 import pygame
 from MenuInstrucoes import start_draw1
 
-#clock = pygame.time.Clock()
-#clock.tick(5)
+# clock = pygame.time.Clock()
+# clock.tick(5)
 pygame.init()
 
-#windows settings
-display_width = 800
-display_height = 600
-
-#colors
-black = (0,0,0)
-white = (255,255,255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
-
-TEXT_SIZE_MENU = 30
-FONT_MENU = 'PacFont'
-TITLE_SIZE = 26
-TEXT_SIZE_GAME = 13
-FONT_GAME = 'arial black'
-
-#window configuration
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Choose character')
-
-#game config
-clock = pygame.time.Clock()
-Running = True
-
-#images
-pacman = pygame.image.load('pacman.png')
-ghost = pygame.image.load('vermelho.png')
-seta = pygame.image.load('seta.png')
-ghost = pygame.transform.scale(ghost, (180, 180))
-seta = pygame.transform.scale(seta, (180, 150))
-
-
-
-
-
-
-xis = (display_width * 0.65)
-ypslon = (display_height * 0.38)
-
-x =  (display_width * 0.1)
-y = (display_height * 0.35)
-
-x_seta = (display_width * 0.13)
-y_seta = (display_height * 0.67)
 
 
 
@@ -60,18 +16,66 @@ def draw_text(message, screen, position, size, font_type, color):
     position[0] = position[0] - text_size[0] // 2
     screen.blit(text, position)
 
+def startDraw():
+    # windows settings
+    display_width = 800
+    display_height = 600
 
-while Running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    # colors
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
 
-    gameDisplay.fill(black)
-    gameDisplay.blit(pacman, (x, y))
-    gameDisplay.blit(ghost, (xis, ypslon))
-    gameDisplay.blit(seta, (x_seta, y_seta))
-    draw_text('CHOOSE YOUR CHARACTER !', gameDisplay, [display_width // 2, display_height // 2 - 140], TITLE_SIZE, FONT_MENU, white)
+    TEXT_SIZE_MENU = 30
+    FONT_MENU = 'PacFont'
+    TITLE_SIZE = 26
+    TEXT_SIZE_GAME = 13
+    FONT_GAME = 'arial black'
 
+    # window configuration
+    gameDisplay = pygame.display.set_mode((display_width, display_height))
+    pygame.display.set_caption('Choose character')
 
-    pygame.display.update()
-    clock.tick(60)
+    # game config
+    clock = pygame.time.Clock()
+    Running = True
+
+    # images
+    pacman = pygame.image.load('pacman.png')
+    ghost = pygame.image.load('vermelho.png')
+    seta = pygame.image.load('seta.png')
+    ghost = pygame.transform.scale(ghost, (180, 180))
+    seta = pygame.transform.scale(seta, (180, 150))
+
+    Running = True
+    xis = (display_width * 0.65)
+    ypslon = (display_height * 0.38)
+
+    x = (display_width * 0.1)
+    y = (display_height * 0.35)
+
+    x_seta = (display_width * 0.13)
+    y_seta = (display_height * 0.67)
+    while Running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    if x_seta == (display_width * 0.13):
+                        x_seta = (display_width * 0.65)
+                if event.key == pygame.K_LEFT:
+                    if x_seta == (display_width * 0.65):
+                        x_seta = (display_width * 0.13)
+                if event.key == pygame.K_ESCAPE:
+                    Running = False
+
+        gameDisplay.fill(black)
+        gameDisplay.blit(pacman, (x, y))
+        gameDisplay.blit(ghost, (xis, ypslon))
+        gameDisplay.blit(seta, (x_seta, y_seta))
+        draw_text('CHOOSE YOUR CHARACTER !', gameDisplay, [display_width // 2, display_height // 2 - 140], TITLE_SIZE,FONT_MENU, white)
+
+        pygame.display.update()
+        clock.tick(60)
