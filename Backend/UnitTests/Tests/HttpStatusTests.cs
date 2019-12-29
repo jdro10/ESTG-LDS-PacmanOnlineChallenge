33cb55a4e1048493225ff1ae5c197eed84cd8213
Jorge Oliveira <jorgedroliveira10@gmail.com>
2019-12-29 18:25:18 +0000

    backend unit tests

--- Backend/UnitTests/Tests/HttpStatusTests.cs
+++ Backend/UnitTests/Tests/HttpStatusTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.Text;
 using API.Controllers;
 using API.Services;
 using API.Models;
@@ -7,14 +8,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class UnitTest1
     {
+        private static readonly HttpClient client = new HttpClient();
 
         [Fact]
-        public void Test1()
+        public void TestRouteAllUsers()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest
+                                           .Create("https://localhost:5001/api/user");
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public void TestRouteUsersRanks()
         {
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest
                                            .Create("https://localhost:5001/api/ranks/allusers");
