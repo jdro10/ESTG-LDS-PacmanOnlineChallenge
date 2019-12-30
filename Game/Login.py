1ca87c42b57475ec1c147a25f6ad2e3a0c98529f
hugocorreia <hrscorreia@gamil.com>
2019-12-30 23:46:51 +0000

    login

--- Game/Login.py
+++ Game/Login.py
@@ -1,14 +1,5 @@
 import pygame
-import requests
 import json
-
-data = {'Username':'user101', 'Password':'pass12345'}
-headers = {'Content-type': 'application/json'}
-
-response = requests.post('https://localhost:5001/api/user/auth', data = json.dumps(data), headers = headers, verify = False)
-
-print(response.status_code)
-print(response.text)
 
 pygame.init()
 screen = pygame.display.set_mode((610, 670))
@@ -24,7 +15,7 @@ class InputBox:
         self.text = text
         self.txt_surface = FONT.render(text, True, self.color)
         self.active = False
-        self.font = pygame.font.SysFont('Arial', 25)
+        self.font = pygame.font.Font('PAC-FONT.TTF', 25)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -56,18 +47,27 @@ class InputBox:
         pygame.draw.rect(screen, self.color, self.rect, 2)
 
     def addText(self):
-        screen.blit(pygame.font.SysFont('PacFont', 25).render(
-            'login', True, (255, 0, 0)), (260, 300))
+        screen.blit(pygame.font.Font('PAC-FONT.TTF', 25).render(
+            'login', True, (255, 0, 0)), (260, 400))
+
+    def addText2(self):
+        screen.blit(pygame.font.Font('PAC-FONT.TTF', 25).render(
+            'ONLINE CHALLENGE', True, (255, 255, 0)), (150, 250))
+
+    def addText3(self):
+        screen.blit(pygame.font.Font('PAC-FONT.TTF', 15).render(
+            'CLICK HERE IF YOU FORGOT YOUR ACCOUNT DETAILS', True, (255, 0, 0)), (20, 600))
 
 
 def main():
     clock = pygame.time.Clock()
-    input_box1 = InputBox(200, 200, 140, 32)
-    input_box2 = InputBox(200, 250, 140, 32)
+    input_box1 = InputBox(220, 300, 140, 32, "Username")
+    input_box2 = InputBox(220, 350, 140, 32, "Password")
     input_boxes = [input_box1, input_box2]
     done = False
 
     while not done:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -77,19 +77,22 @@ def main():
         for box in input_boxes:
             box.update()
 
-        screen.fill((30, 30, 30))
+        screen.fill((0,0,0))
         for box in input_boxes:
+            
             box.draw(screen)
-
-        button = pygame.Rect(260, 300, 100, 40)
-        pygame.draw.rect(screen, [255, 255, 255], button)
+        
+        InputBox.addText3(screen)
+        InputBox.addText2(screen)
+        button = pygame.Rect(260, 400, 100, 40)
+        pygame.draw.rect(screen, [0, 0, 0], button)
         InputBox.addText(screen)
-        img = pygame.image.load('img/logo.png')
-        screen.blit(pygame.transform.scale(img, (200, 150)), (200, 10))
+        img = pygame.image.load('img/logo.jpg')
+        screen.blit(pygame.transform.scale(img, (610, 250)), (0, 0))
         pygame.display.flip()
         clock.tick(30)
-
-
+        
+        
 if __name__ == '__main__':
     main()
     pygame.quit()
